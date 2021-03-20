7013d2ed4f16b7ef06183f6619ba242dffcb8715
seshadwi <seshadwi@gmail.com>
2021-03-20 21:53:33 +0700

    Praktikum 3: Change login using username

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,13 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+
+                    <table class="table table-responsive">
+                        <tr><th>Username</th><th>:</th><th>{{ $user->username }}</th></tr>
+                        <tr><th>Name</th><th>:</th><th>{{ $user->name }}</th></tr>
+                        <tr><th>Email</th><th>:</th><th>{{ $user->email }}</th></tr>
+                        <tr><th>Created At</th><th>:</th><th>{{ $user->created_at }}</th></tr>
+                    </table>
                 </div>
             </div>
         </div>
